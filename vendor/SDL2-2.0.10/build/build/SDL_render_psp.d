df4e791629e2b8551459ca0066b79d9604854104
mobile_ragnar <mobile_ragnar@git.krusesec.com>
2019-09-17 14:12:05 +0000

    remove temp files

--- vendor/SDL2-2.0.10/build/build/SDL_render_psp.d
+++
@@ -1,9 +0,0 @@
-build/SDL_render_psp.lo: \
- /home/ragnar/projects/github/i6_engine/vendor/SDL2-2.0.10/src/render/psp/SDL_render_psp.c \
- /home/ragnar/projects/github/i6_engine/vendor/SDL2-2.0.10/src/render/psp/../../SDL_internal.h \
- /home/ragnar/projects/github/i6_engine/vendor/SDL2-2.0.10/src/render/psp/../../dynapi/SDL_dynapi.h \
- /home/ragnar/projects/github/i6_engine/vendor/SDL2-2.0.10/src/render/psp/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h \
- /home/ragnar/projects/github/i6_engine/vendor/SDL2-2.0.10/include/SDL_platform.h \
- /home/ragnar/projects/github/i6_engine/vendor/SDL2-2.0.10/include/begin_code.h \
- /home/ragnar/projects/github/i6_engine/vendor/SDL2-2.0.10/include/close_code.h
